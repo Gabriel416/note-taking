8b01a44b74a3e807695214b2b8ed6ebb30dbfd5c
Gabriel Ferguson <gferguson954@gmail.com>
2021-04-04 01:40:48 -0500

    note taking ui

--- server/server.js
+++
@@ -1,7 +0,0 @@
-const { ApolloServer } = require('apollo-server');
-const { resolvers, typeDefs } = require('./schema');
-
-const server = new ApolloServer({ resolvers, typeDefs });
-server.listen({ port: 4000 }, () => {
-    console.log('listening on post 4000')
-});
---
+++ src/components/NotesContainer/index.js
@@ -0,0 +1,61 @@
+import React from 'react';
+import classnames from 'classnames';
+import { useMutation, NetworkStatus } from '@apollo/client';
+import { List } from 'react-content-loader';
+import { Editor, convertFromRaw, EditorState } from 'draft-js';
+
+import { DELETE_NOTE } from '../../Mutations';
+
+import './notes_container.css'
+
+const NotesContainer = ({
+    error,
+    loading,
+    networkStatus,
+    refetch,
+    notes,
+    selectedNote,
+    setSelectedNote
+}) => {
+    const [deleteNote, { loading: mutationLoading, error: mutationError }] = useMutation(DELETE_NOTE);
+
+    const handleDelete = async (id) => {
+        await deleteNote({
+            variables: {
+                id
+            }
+        })
+        refetch();
+    }
+
+    const renderNotes = () => {
+        if (loading || mutationLoading || networkStatus === NetworkStatus.refetch) {
+            return Array.from(Array(notes.length)).map((_, i) => <List className="box" key={i} />)
+        } else if (error) {
+            return <p>{`Error! ${error}`}</p>
+        }
+
+        return notes.map((note, i) => {
+            const isActive = selectedNote ? selectedNote.id === note.id : false;
+            const parsedPost = EditorState.createWithContent(convertFromRaw(JSON.parse(note.body)));
+            return (
+                <section key={i} className={classnames('box', isActive ? 'active-post' : '')}>
+                    <div onClick={() => setSelectedNote(note)}>
+                        <Editor editorState={parsedPost} readOnly />
+                    </div>
+                    <button className="btn small" onClick={() => handleDelete(note.id)}>Delete Note</button>
+                    {mutationError && <p className="error">Error occurred when deleting note</p>}
+                </section>
+            )
+        })
+    }
+
+    return (
+        <div className="notes_container_wrapper">
+            <h2 className="ml-1">Notes</h2>
+            {renderNotes()}
+        </div>
+    )
+}
+
+export default NotesContainer;
